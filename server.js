c286f48479d1571ba7fa6ea2821b94e33feec8a3
nithu-balachandran <nithu.balachandran@gmail.com>
2016-09-25 03:30:08 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,6 +5,19 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+
+app.get('/article-one',function(req,res){
+    res.send("first article");
+});
+
+app.get('/article-two',function(req,res){
+    res.send("second article");
+});
+
+app.get('/article-three',function(req,res){
+    res.send("third article");
+});
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
